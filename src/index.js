d52c501414877460d4862041bb8b885c74264c58
Kool Havis <havisk@gmail.com>
2017-08-12 23:59:22 -0400

    added form, formControl, button and css \

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app'
 
 ReactDOM.render(
-    <div>ageteller</div>,
+    <App/>,
     document.getElementById('root')
 );
