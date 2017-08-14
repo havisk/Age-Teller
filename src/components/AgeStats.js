86a49b526b02aaa1a3d023a17415f802697becc3
Kool Havis <havisk@gmail.com>
2017-08-13 21:19:22 -0400

    added date time computing difference

---
+++ src/components/AgeStats.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class AgeStates extends Component {
+    timeSince(date) {
+        let today = new Date().getTime();
+        let otherDate = new Date(date).getTime();
+        let diff = Math.abs(today - otherDate);
+
+        let days = Math.floor(diff/(1000*3600*24));
+        let years = Math.floor(days/365);
+        days -=years*365;
+        let months = Math.floor(days/31);
+        days -= months*31;
+
+
+        return `${years} years, ${months} months, and ${days} days`
+
+    };
+
+    render() {
+        return(
+            <div>
+                <h3>{this.props.date}</h3>
+                <h4>Congrats You are {this.timeSince((this.props.date))}!!</h4>
+            </div>
+        )
+    };
+}
+
+
+export default AgeStates;
--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,41 @@
 import React, {Component} from 'react';
 import{Form, FormControl, Button} from 'react-bootstrap'
 import './app.css';
+import AgeStates from './AgeStats';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            newDate: '',
+            birthday: '1974-08-29'
+        }
+    }
+
+    changeBirthday() {
+        console.log(this.state);
+        this.setState({birthday: this.state.newDate});
+    }
+
     render() {
         return (
             <div className="App">
                 <Form inline>
                     <h2>Input Your Birthday!</h2>
-                    <FormControl type="date">
+                    <FormControl
+                        type="date"
+                        onChange={event =>
+                            this.setState({newDate:
+                            event.target.value})}
+                    >
                     </FormControl>
                     {' '}
-                    <Button>
+                    <Button onClick ={() =>
+                        this.changeBirthday()} >
                         Submit
                     </Button>
+                    <AgeStates date={this.state.birthday}/>
                 </Form>
             </div>
         )
